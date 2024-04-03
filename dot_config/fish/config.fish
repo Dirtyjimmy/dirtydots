73fac5b85854e931533ecd2e480e45a4fab87128
dirtyjimmy <jimiolan@icloud.com>
2024-04-03 22:16:19 +0300

    Initial commit

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+zoxide init fish | source    # Commands to run in interactive sessions can go here
+end
+alias config='/usr/bin/git --git-dir=/home/drtjm/.hypr-dots/.git/ --work-tree=/home/drtjm'
+alias config='/usr/bin/git --git-dir=/home/drtjm/.hypr-dots/.git/ --work-tree=/home/drtjm'
