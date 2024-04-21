c132b6f95dbf13e43afe370a34cc3d294e4bf331
dirtyjimmy <jimiolan@icloud.com>
2024-04-21 23:54:47 +0300

    Updated nvim

--- dot_config/fish/config.fish
+++
@@ -1,5 +0,0 @@
-if status is-interactive
-zoxide init fish | source    # Commands to run in interactive sessions can go here
-end
-alias config='/usr/bin/git --git-dir=/home/drtjm/.hypr-dots/.git/ --work-tree=/home/drtjm'
-alias config='/usr/bin/git --git-dir=/home/drtjm/.hypr-dots/.git/ --work-tree=/home/drtjm'
